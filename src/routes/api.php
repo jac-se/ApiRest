199a8362254c8b4fee3ee5fd2f79d2dd74994923
Jesús Arturo Cisneros Cantero <chucho.cisneros@hotmail.com>
2026-01-15 20:36:28 +0000

    se ajustaron seguridad keyclook

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,24 +1,47 @@
-<?php 
-use Illuminate\Http\Request; 
-use Illuminate\Support\Facades\Route; 
-// Agregar el controlador EventoController 
-use App\Http\Controllers\EventoController; 
-/** 
-* Rutas para el recurso Evento. 
-*/ 
-// Recuperar todos los eventos 
-Route::get('/eventos', [EventoController::class, 'index']); 
+<?php
 
-// Almacenar un evento nuevo 
-Route::post('/eventos', [EventoController::class, 'store']); 
-// Recuperar un evento específico 
-Route::get('/eventos/{id}', [EventoController::class, 'show']); 
-// Actualizar un evento específico 
-Route::put('/eventos/{id}', [EventoController::class, 'update']);
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
+use App\Http\Controllers\AsistenteController;
+use App\Http\Controllers\PonenteController;
 
-// Eliminar un evento específico 
-Route::delete('/eventos/{id}', [EventoController::class, 'destroy']); 
+/*
+|--------------------------------------------------------------------------
+| Rutas públicas
+|--------------------------------------------------------------------------
+*/
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::get('/eventos', [EventoController::class, 'index']);
+Route::get('/eventos/{id}', [EventoController::class, 'show']);
+
+/*
+|--------------------------------------------------------------------------
+| Rutas protegidas (auth:api)
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware('auth:api')->group(function () {
+
+    // Eventos
+    Route::post('/eventos', [EventoController::class, 'store']);
+    Route::put('/eventos/{id}', [EventoController::class, 'update']);
+    Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+
+    // Ponentes
+    Route::post('/ponentes', [PonenteController::class, 'store']);
+    Route::put('/ponentes/{id}', [PonenteController::class, 'update']);
+    Route::delete('/ponentes/{id}', [PonenteController::class, 'destroy']);
+
+    // Asistentes
+    Route::get('/asistentes', [AsistenteController::class, 'index']);
+    Route::post('/asistentes', [AsistenteController::class, 'store']);
+    Route::get('/asistentes/{id}', [AsistenteController::class, 'show']);
+    Route::put('/asistentes/{id}', [AsistenteController::class, 'update']);
+    Route::delete('/asistentes/{id}', [AsistenteController::class, 'destroy']);
+
+    // Usuario autenticado
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
